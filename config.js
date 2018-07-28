6a2d1d5009d1c58610bd26bae3477176bf1955ff
pratik-dev <40396898+pratik-dev@users.noreply.github.com>
2018-07-28 11:58:14 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    FB_PAGE_TOKEN: '',
-    FB_VERIFY_TOKEN: '',
-    API_AI_CLIENT_ACCESS_TOKEN: '',
-    FB_APP_SECRET: '',
-    SERVER_URL: "",
+    // FB_PAGE_TOKEN: '',
+    // FB_VERIFY_TOKEN: '',
+    API_AI_CLIENT_ACCESS_TOKEN: 'f00d3f8b0a7c435cbe24dd356f924138',
+    // FB_APP_SECRET: '',
+    SERVER_URL: 'https://sfcc-api-integration.herokuapp.com/',
 };
