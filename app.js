49444b753dd18d27411035bf6d6cd5d4a8dd0720
pratik-dev <40396898+pratik-dev@users.noreply.github.com>
2018-09-20 12:51:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ app.post('/webhook/', (req, res) => {
 													"data": {
 															"@type": "type.googleapis.com/google.actions.v2.PermissionValueSpec",
 															"optContext": "To process your order, ",
-															"permissions": ["DEVICE_PRECISE_LOCATION"]
+															"permissions": ["DEVICE_COARSE_LOCATION"]
 																	}
 																}
 															}
@@ -90,10 +90,10 @@ app.post('/webhook/', (req, res) => {
 								console.log("After entering check permission", JSON.stringify(req.body));
 								console.log(req.body.originalRequest.data.inputs[0].arguments[0].boolValue);
 								if(req.body.originalRequest.data.inputs[0].arguments[0].boolValue){
-								var uLat=req.body.originalRequest.data.device.location.coordinates.latitude;
-								var uLng=req.body.originalRequest.data.device.location.coordinates.longitude;
-								var ulat = 12.9666400;
-								var ulng = 77.7232870;
+								//var uLat=req.body.originalRequest.data.device.location.coordinates.latitude;
+								//var uLng=req.body.originalRequest.data.device.location.coordinates.longitude;
+								var uLat = 12.9666400;
+								var uLng = 77.7232870;
 								qsr.nearestStoreService(uLat, uLng, (error, storeResult) =>{
 									if(error){
 										console.log(error);
