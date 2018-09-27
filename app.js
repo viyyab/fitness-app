5f200039fc99955bfcb3f7a012a9bbb997d33d52
pratik-dev <40396898+pratik-dev@users.noreply.github.com>
2018-09-27 21:37:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -231,18 +231,18 @@ app.post('/webhook/', (req, res) => {
 		case 'OrderConfirmed': {
  					console.log('In action OrderConfirmed');
  					if(isDefined(actionName)){
- 						var text = '';
+ 						//var text = '';
 						qsr.settingDeliveryModeService(access_token, cartId, email, (error,result)=> {
 						 if(error){
 							console.log(error);
 							}else {
 								console.log(result);
-								qsr.addCardPaymentService(access_token, cartId, email, cardId, (error, result)=>{
+								qsr.addCardPaymentService(access_token, cartId, email, cardId, (error, paymentResult)=>{
 									if(error){
 										console.log(error);
 									}else {
 										console.log('Payment details added');
-										qsr.placeOrderService(access_token, cartId, email, storeId, (error, result) =>{
+										qsr.placeOrderService(access_token, cartId, email, storeId, (error, orderResult) =>{
 											if(error){
 											}else{
 												text= `Your order has been submitted. Your order code is ${result.code}. Please provide this code when you get to the restaurant and they'll get your order started. I will also text it to you for reference. Thank you for your order!`;
