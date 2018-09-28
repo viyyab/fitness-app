c29c339a5055cfd6141fe86ae8385e5830381150
pratik-dev <40396898+pratik-dev@users.noreply.github.com>
2018-09-28 12:44:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -253,16 +253,16 @@ app.post('/webhook/', (req, res) => {
 							}else{
 								console.log(orderResult.code);
 								orderCode=orderResult.code;
-								}
-						        });
-						  text= `Your order has been submitted. Your order code is ${orderCode}. Please provide this code when you get to the restaurant and they'll get your order started. I will also text it to you for reference. Thank you for your order!`;
+								text= `Your order has been submitted. Your order code is ${orderCode}. Please provide this code when you get to the restaurant and they'll get your order started. I will also text it to you for reference. Thank you for your order!`;
 								 messageData = {
 										speech: text,
 										displayText: text
 										}
-								   res.send(messageData);
-					 	             }
-						        }
+								 }
+						           });
+							res.send(messageData);
+						     }
+						 }
  					 break;
 		
 		 default:
