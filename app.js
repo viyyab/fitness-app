1c1bf939c86ae6f0cb4fdb215b4dd48ffce01e47
pratik-dev <40396898+pratik-dev@users.noreply.github.com>
2018-09-28 17:01:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const qsr= require('./qsr-apis.js');
 const request= require('request');
 const DialogflowApp = require('actions-on-google').DialogflowApp;
 const app = express();
-var async = require('async');
+const async = require('async');
 var access_token;
 var refresh_token;
 var text = '';
