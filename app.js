eac22634b94a87ecf8970523f444d7edf5cad2a5
pratik-dev <40396898+pratik-dev@users.noreply.github.com>
2018-09-28 17:57:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -248,14 +248,14 @@ app.post('/webhook/', (req, res) => {
  					console.log('In action OrderConfirmed');
  					if(isDefined(actionName)){
  						console.log(cartId+'   '+cardId);
-							function myFunc(orderCode) {
-								text= `Your order has been submitted. Your order code is ${orderCode}. Please provide this code when you get to the restaurant and they'll get your order started. I will also text it to you for reference. Thank you for your order!`
-									 messageData = {
-											speech: text,
-											displayText: text
-											}
-									res.send(messageData);	
-							}
+						function myFunc(orderCode) {
+							text= `Your order has been submitted. Your order code is ${orderCode}. Please provide this code when you get to the restaurant and they'll get your order started. I will also text it to you for reference. Thank you for your order!`
+								 messageData = {
+										speech: text,
+										displayText: text
+										}
+								res.send(messageData);	
+						}
 // 						exports.asyncSeries = function (req, res, callback) { 
 // 						async.series([
 // 							//console.log('Inside series call');
@@ -294,8 +294,7 @@ app.post('/webhook/', (req, res) => {
 // 							}
 // 							callback();
 // 						   }
-// 						)
-// 						} 					
+// 						)						} 					
 						qsr.placeOrderService(access_token, cartId, email, storeId, (error, orderResult) =>{
 							if(error){
 								console.log(error);
