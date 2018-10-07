e01e8009287a3651205e79c7f305cd57581afe5a
unknown <pratikb365@gmail.com>
2018-10-07 21:31:22 +0530

    commit

--- app.js
+++ app.js
@@ -349,7 +349,7 @@ app.post('/webhook/', (req, res) => {
  					if(isDefined(actionName)){
  						console.log(cartId+'   '+cardId);
 						function myFunc(orderCode) {
-							text= `Your order has been submitted. Your order code is ${orderCode}. Please provide this code when you get to the restaurant and they'll get your order started. I will also text it to you for reference. Thank you for your order!`
+							text= `Your order has been submitted. Your order code is ${orderCode.substr(4,4)}. Please provide this code when you get to the restaurant and they'll get your order started. I will also text it to you for reference. Thank you for your order!`
 								 messageData = {
 										speech: text,
 										displayText: text
