20e6ac1e7eb13b8fcbfd928efe9ce4b7557e00d3
unknown <pratikb365@gmail.com>
2018-10-20 02:59:25 +0530

    commit

--- rpc.js
+++ rpc.js
@@ -1,6 +1,6 @@
 const xmlrpc = require('xmlrpc');
 const xml2js = require('xml2js');
- console.log("XML RPC client call")
+ //console.log("XML RPC client call")
 // // Creates an XML-RPC server to listen to XML-RPC method calls
 // var server = xmlrpc.createServer({ host: 'localhost', port: 9090 })
 // // Handle methods not found
@@ -23,13 +23,17 @@ const xml2js = require('xml2js');
 var xmlRpcClientService = (dataResult, callback) => {
   // Creates an XML-RPC client. Passes the host information on where to
   console.log("Entered RPC client call "+ dataResult)
-  var client = xmlrpc.createClient("http://54.211.9.131:8088/RPC2")
-
+  var client = xmlrpc.createClient('http://54.211.9.131:8088/RPC2');
+  console.log("Client created");
   // Sends a method call to the XML-RPC server
   client.methodCall('DoFoeStoreFromFile', [dataResult], function (error, value) {
     console.log("Into RPC client method call")
     if(error){
       console.log(error);
+      console.log('error:', error);
+      console.log('req headers:', error.req && error.req._header);
+      console.log('res code:', error.res && error.res.statusCode);
+      console.log('res body:', error.body);
     } else {
       console.log('Method response for \'DoFoeStoreFromFile\': ' + value);
       callback("XML to RPC client hit successful !")
