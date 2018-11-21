bc4c6b328667be9ad5c744a2e4fe76a8109ace60
pratik-dev <40396898+pratik-dev@users.noreply.github.com>
2018-11-21 16:05:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -179,8 +179,13 @@ app.post('/webhook/', (req, res) => {
 		case 'shoes-in-stock-ask': {
 		 				console.log('In shoes-in-stock-ask');
 		 					if(isDefined(actionName)){
-								
-								//mailer.sendMailService(emailId, customerName);
+								text: "Sure, I'll inform the store manager. Your shoes will be ready on time. Probably don't use them for your next trail as the distance is too long for brand new shoes. By the way do you want to check how you used your last pair ?";
+								messageData = {
+										speech: text,
+										displayText: text
+										}
+								res.send(messageData);	
+								mailer.sendMailService(emailId, customerName);
 							     }
 							}
 		 				break;
