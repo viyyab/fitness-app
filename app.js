0bec6d5d22395208cfa4052b77ce986163056f67
viyyab <48179978+viyyab@users.noreply.github.com>
2019-03-12 14:59:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -199,9 +199,9 @@ app.post('/webhook/', (req, res) => {
 						console.log(req.body.result.parameters.FirstName);
 						//console.log(' req.body.result.contexts[0].parameters : ' + req.body.result.contexts[0].parameters);
 						var firstName = req.body.result.parameters.FirstName;
-						var lastName = req.body.result.parameters.lastname;
-						var email = req.body.result.parameters.email;
-						var company = req.body.result.parameters.company;						
+						var lastName = req.body.result.parameters.LastName;
+						var email = req.body.result.parameters.Email;
+						var company = req.body.result.parameters.Company;						
 						sfcc.createLead(firstName, lastName, email, company, (error, leadResult)=> {
 							if(error){
 								console.log(error);
